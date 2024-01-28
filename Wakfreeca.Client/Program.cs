67b7190bb3bbde44d56b49544491e1dc9a6224fa
Dareum Nam <namdalen1115@gmail.com>
2024-01-28 22:08:52 +0900

    프로젝트 구조 변경 및 인터렉티브웹어셈블리 렌더모드 추가

--- Wakfreeca.Client/Program.cs
+++ Wakfreeca.Client/Program.cs
@@ -1,9 +1,13 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddScoped<MudThemeProvider>();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
